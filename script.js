c06f0e4cdea0ad9e0de067f9a7a35dd1292da86a
Kirill <stoporenko.k@gmail.com>
2025-03-12 01:23:20 +0700

    Schema Validation

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ async function run() {
 		const user = await User.create({
 			name: "Kyle",
 			age: 26,
+			email: "test@mail.com",
 			hobbies: ["Weight Lifting", "Bowling"],
 			address: {
 				street: "Main St"
 			}
 		});
+		user.createdAt = 5;
+		await user.save();
 		console.log(user);
 	} catch (e) {
 		console.log(e.message);
